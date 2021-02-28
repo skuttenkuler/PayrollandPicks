56111889e2062c7ccbc053c431698784e2d7499a
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-27 16:50:15 -0800

    new install, new env, new set up, poetry, admin user set up

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -11,11 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-from dotenv import load_dotenv
-
-project_folder = os.path.expanduser('~/backend')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
--- backend/payrollsandpicks/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PayrollsandpicksConfig(AppConfig):
-    name = 'payrollsandpicks'
