e99a953d5a33118863d5417285a35a6894a8bd78
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-27 23:16:09 -0800

    super created, postgres db created and connected

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'pp_db',
+    'USER': 'pp_admin',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '',
+}
+
 }
 
 
