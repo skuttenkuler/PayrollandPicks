94005538d16e53ba48e8074ec5597ffc59655387
skuttenkuler <sam.kuttenk@gmail.com>
2021-06-23 14:40:24 -0500

    single team and single player querys created

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1ee45f522a29d5b949dc";
+/******/ 	var hotCurrentHash = "3dfb0277c656863bba14";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
