d7b8205b0435b95440a1f4c91216b4e74f3b3284
skuttenkuler <sam.kuttenk@gmail.com>
2021-06-24 22:03:50 -0500

    dynamic routing for team and player, working out query and param. aql query passing query param

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3dfb0277c656863bba14";
+/******/ 	var hotCurrentHash = "1d5b708f8755a197104c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/config/index.ts
@@ -0,0 +1,3 @@
+const dev: boolean = process.env.NODE_ENV !== 'production'
+
+export const server = 'http://localhost:3000'
